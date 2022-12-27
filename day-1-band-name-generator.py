df8ce4ac9b90d65c9d3c55158ad1bcb46cf6da8f
caralifarrell <79096101+caralifarrell@users.noreply.github.com>
2022-12-26 20:00:39 -0500

    Add files via upload

--- day-1-band-name-generator.py
+++ day-1-band-name-generator.py
@@ -1,3 +1,6 @@
+# Project 1: Band Name Generator
+# December 23, 2022
+
 print("Welcome to the Band Name Generator.")
 city = input("What's the name of the city you grew up in? \n")
 pet = input("What's your pet's name? \n")
