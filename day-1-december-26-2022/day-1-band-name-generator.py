56a5b0d5dd0a892aff1722bbb6d53bc39d352e38
caralifarrell <79096101+caralifarrell@users.noreply.github.com>
2022-12-28 17:17:16 -0500

    Delete day-1-december-26-2022 directory

--- day-1-december-26-2022/day-1-band-name-generator.py
+++
@@ -1,7 +0,0 @@
-# Project 1: Band Name Generator
-# December 26, 2022
-
-print("Welcome to the Band Name Generator.")
-city = input("What's the name of the city you grew up in? \n")
-pet = input("What's your pet's name? \n")
-print("Your band name could be " + str(city) + " " + str(pet))
