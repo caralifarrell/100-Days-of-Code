6865a8a1f22ff6536e4c64ddf50b037db8eba249
caralifarrell <79096101+caralifarrell@users.noreply.github.com>
2022-12-28 18:39:34 -0500

    Update day-3-project-treasure-island.py

--- day-3-december-28-2022/day-3-project-treasure-island.py
+++ day-3-december-28-2022/day-3-project-treasure-island.py
@@ -1,1 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
 
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+direction = input("You finally got to the end of your path and it now splits into two, going opposite directions. Do you want to go left or right? ")
+direction = direction.lower()
+if direction == "left":
+    water = input ("You walked for a few minutes and now you see a body of water in front of you. Do you want to swim or wait? ")
+    water = water.lower()
+    if water == "wait":
+        door = input("There are now three doors in front of you. Which door would you like to open? Red, Yellow or Blue? ")
+        door = door.lower()
+        if door == "red":
+            print("Oh no! There was a fire behind this door and you walked right in! Game Over.")
+        elif door == "blue":
+            print("Oh no! A hungry beast was waiting for you! It devoured you... Game Over.")
+        elif door == "yellow":
+            print("Yay! You found th treasure! You Win!")
+        else:
+            print("Game Over.")
+    else: 
+        print("You started swimming... but then you attracted some sharks! They ate you! Game Over.")
+else:
+    print("You started walking... and you fell in a hole! Game Over.")
