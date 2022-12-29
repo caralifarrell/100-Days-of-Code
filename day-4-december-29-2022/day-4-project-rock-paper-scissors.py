2c2411bf7012e820e338a36285d2bc848cb3b1c5
Cara-Li Farrell <79096101+caralifarrell@users.noreply.github.com>
2022-12-29 17:36:15 -0500

    Update day-4-project-rock-paper-scissors.py

--- day-4-december-29-2022/day-4-project-rock-paper-scissors.py
+++ day-4-december-29-2022/day-4-project-rock-paper-scissors.py
@@ -34,38 +34,40 @@ user_choice = int(
         'What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'
     ))
 
-if user_choice == 0:
-    print(rock)
-elif user_choice == 1:
-    print(paper)
-elif user_choice == 2:
-    print(scissors)
-
 computer_choice = random.randint(0, 2)
 
-if computer_choice >= 3:
-  print("")
-elif computer_choice == 0:
-    print(f"Computer chose:\n{rock}")
-elif computer_choice == 1:
-    print(f"Computer chose:\n{paper}")
-elif computer_choice == 2:
-    print(f"Computer chose:\n{scissors}")
-
-if user_choice == computer_choice:
-  print("It's a tie. Nobody wins.")
-elif user_choice == 0:
-  if computer_choice == 1:
-    print("Rock gets covered by paper. You lose.")
-  elif computer_choice == 2:
-    print("Rock crushes scissors. You win.")
+if user_choice == 0:
+    print(rock)
+    if computer_choice == 0:
+      print(f"Computer chose:\n{rock}")
+      print("It's a tie. Nobody wins.")
+    elif computer_choice == 1:
+      print(f"Computer chose:\n{paper}")
+      print("Rock gets covered by paper. You lose.")
+    elif computer_choice == 2:
+      print(f"Computer chose:\n{scissors}")
+      print("Rock crushes scissors. You win.")
 elif user_choice == 1:
-  if computer_choice == 0:
-    print("Paper covers rock. You win.")
-  elif computer_choice == 2:
-    print("Paper gets cut by scissors. You lose.")
+    print(paper)
+    if computer_choice == 0:
+      print(f"Computer chose:\n{rock}")
+      print("Paper covers rock. You win.")
+    elif computer_choice == 1:
+      print(f"Computer chose:\n{paper}")
+      print("It's a tie. Nobody wins.")
+    elif computer_choice == 2:
+      print(f"Computer chose:\n{scissors}")
+      print("Paper gets cut by scissors. You lose.")
 elif user_choice == 2:
-  if computer_choice == 0:
-    print("Scissors, gets crushed by rock. You lose.")
-  elif computer_choice == 1:
-    print("Scissors cuts paper. You win.")
+    print(scissors)
+    if computer_choice == 0:
+      print(f"Computer chose:\n{rock}")
+      print("Scissors gets crushed by rock. You lose.")
+    elif computer_choice == 1:
+      print(f"Computer chose:\n{paper}")
+      print("Scissors cuts paper. You win.")
+    elif computer_choice == 2:
+      print(f"Computer chose:\n{scissors}")
+      print("It's a tie. Nobody wins.")
+else: 
+  print("That is not a valid play. You lose.")
