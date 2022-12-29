54451d0461420a36012e1a5aa6e2e802e5dff434
Cara-Li Farrell <79096101+caralifarrell@users.noreply.github.com>
2022-12-29 14:56:46 -0500

    Update exercise-1-heads-or-tails.py

--- day-4-december-29-2022/exercise-1-heads-or-tails.py
+++ day-4-december-29-2022/exercise-1-heads-or-tails.py
@@ -1,1 +1,9 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
 
+import random
+
+heads_tails = random.choice(['Heads','Tails'])
+print(heads_tails)
