e79411e6323d0289e281c91b65a76506a8de85cf
Cara-Li Farrell <79096101+caralifarrell@users.noreply.github.com>
2022-12-30 11:43:11 -0500

    Create day-5-project-password-generator.py
    Head over to replit.com
    https://replit.com/@appbrewery/password-generator-start?v=1
    Coding Rooms is very handy for our exercises. However, the end-of-day projects are more free-form. You can experiment and play with this project. Customise it. Get as creative as you like. There is no exact output for you to match. There are no tests for you to pass. These assignments are not graded.

---
+++ day-5-december-30-2022/day-5-project-password-generator.py
@@ -0,0 +1,37 @@
+#Password Generator Project
+import random
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+randomised_letters = random.choices(letters, k = nr_letters)
+randomised_symbols = random.choices(symbols, k = nr_symbols)
+randomised_numbers = random.choices(numbers, k = nr_numbers)
+
+generated_password = randomised_letters + randomised_symbols + randomised_numbers
+ordered = ''.join(generated_password)
+print(ordered)
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+import random
+
+generated_password_randomised = randomised_letters + randomised_symbols + randomised_numbers
+random.shuffle(generated_password_randomised)
+shuffled = ''.join(generated_password_randomised)
+print(shuffled)
