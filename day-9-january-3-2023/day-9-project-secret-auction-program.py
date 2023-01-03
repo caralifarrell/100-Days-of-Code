109b50500002cebc26920ab222cf2244d1834e3d
Cara-Li Farrell <79096101+caralifarrell@users.noreply.github.com>
2023-01-03 17:02:44 -0500

    Update day-9-project-secret-auction-program.py

--- day-9-january-3-2023/day-9-project-secret-auction-program.py
+++ day-9-january-3-2023/day-9-project-secret-auction-program.py
@@ -1,1 +1,30 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
 
+from art import logo
+print(logo)
+
+def highest_bidder(bidding_list):
+  highest_bid = 0
+  winner = ""
+  for bidder in bidding_list:
+    bid_amount = bidding_list[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+    
+  
+more_bidding = True
+
+while more_bidding:
+  bids = {}
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  more_bidders = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if more_bidders == "yes":
+    clear()
+  elif more_bidders == "no":
+    more_bidding = False
+    highest_bidder(bids)
